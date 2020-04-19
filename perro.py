35e38a1b3ec710a467b4f689470ceeb13df3235f
Moks8 <cristinagutierrezrub@gmail.com>
2020-04-19 17:01:46 +0200

    Test

--- perro.py
+++ perro.py
@@ -1,6 +1,53 @@
+class Dog():
+    def __init__(self):
+        self.nombre=""
+        self.edad=None
+        self.peso = None
+    def ladrar(self):
+        if self.peso == None:
+            print("Soy un fantasma")
+            
+        if self.peso >= 8:
+            print("GUAU,GUAU")
+        else:
+            print ("Guau,Guau")
+
 class Perro():
     def __init__(self,nombre,edad,peso):
-        self.nombre = so
+        self.nombre = nombre
         self.edad = edad
-        self.peso= pe
+        self.peso = peso
+    def ladrar(self):
+        if self.peso >= 8:
+            print("GUAU,GUAU")
+        else:
+            print ("Guau,Guau")
+            
+            
+    def __str__(self):
+        return"Soy el perro{}".format(self,nombre)
+    
+class PerroAsistencia(Perro):
+    def __init__(self,nombre,edad,peso,amo):
+        Perro.__init__(self,nombre,edad,peso)
+        self.amo =amo
+        self.__trabajando =  False
+        
+    def __str__(self):
+        return"Perro de asistencia de {}".format(self.amo)
+    def pasear(self):
+        print("{} ayudo a mi dueño, {} a pasear".format(self.nombre,self.amo))
+        
+    def ladrar (self):
+        if self.__trabajando:
+            print("shhhh,no puedo ladrar")
+        else:
+            Perro.ladrar(self)
+            
+    def trabajando(self,valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self__Trabajando = valor
+        
     
